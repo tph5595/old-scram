e8ecb25b329994af8ae6e2ea50a2aec4d5bc6c4e
j105rob <j105.rob@gmail.com>
2013-06-20 10:35:21 -0400

    Updated Poll MSg in UI

--- http/js/dojo/scram/poll.js
+++ http/js/dojo/scram/poll.js
@@ -6,47 +6,63 @@ define(["dojo/_base/lang", "dojo/on", "dojo/_base/declare", "dijit/_WidgetBase",
 		_socket : null,
 		parent : null,
 
-
 		constructor : function(args) {
 			this._socket = args.socket;
 			this.parent = args.parent;
+			this._socket.on("message", lang.hitch(this, this.pollMsg));
 		},
 		postCreate : function() {
-				this.mwh = new domConstruct.create('div', {
-					id : 'mwh',
-					'class': 'mwh statsbarelements'
-				}, this.parent);
-				
-				this.simtime = new domConstruct.create('div', {
-					id : 'simtime',
-					'class':"simtime statsbarelements"
-				}, this.parent);
-				this.rcspressure = new domConstruct.create('div', {
-					id : 'rcspressure',
-					'class': "rcspressure statsbarelements"
-				}, this.parent);
-				this.boilingtemp = new domConstruct.create('div', {
-					id : 'boilingtemp',
-					'class': "boilingtemp statsbarelements"
-				}, this.parent);
-				this.workers = new domConstruct.create('div', {
-					id : 'workers',
-					'class': 'workers statsbarelements'
-				}, this.parent);
-				this.risk = new domConstruct.create('div', {
-					id : 'risk',
-					'class': 'risk'
-				}, this.parent);
-				this.generalPowerOutput = new domConstruct.create('div', {
-					id : 'generalPowerOutput'
-				}, this.parent);
-				this.simtime.innerHTML = "123456";
-				this.rcspressure.innerHTML = "123456";
-				this.boilingtemp.innerHTML = "123456";
-				this.workers.innerHTML = "123456";
-				this.risk.innerHTML = "123";
-				this.mwh.innerHTML = "123456";
+			this.mwh = new domConstruct.create('div', {
+				id : 'mwh',
+				'class' : 'mwh statsbarelements'
+			}, this.parent);
+
+			this.simtime = new domConstruct.create('div', {
+				id : 'simtime',
+				'class' : "simtime statsbarelements"
+			}, this.parent);
+			this.rcspressure = new domConstruct.create('div', {
+				id : 'rcspressure',
+				'class' : "rcspressure statsbarelements"
+			}, this.parent);
+			this.boilingtemp = new domConstruct.create('div', {
+				id : 'boilingtemp',
+				'class' : "boilingtemp statsbarelements"
+			}, this.parent);
+			this.workers = new domConstruct.create('div', {
+				id : 'workers',
+				'class' : 'workers statsbarelements'
+			}, this.parent);
+			this.risk = new domConstruct.create('div', {
+				id : 'risk',
+				'class' : 'risk'
+			}, this.parent);
+			this.generalPowerOutput = new domConstruct.create('div', {
+				id : 'generalPowerOutput'
+			}, this.parent);
 			this.inherited(arguments);
+		},
+		pollMsg : function() {
+
+			try {
+				var obj = JSON.parse(event.data);
+				this.mwh.innerHTML = Math.round(obj.mwh);
+				reactorTemp.innerHTML = Math.round(obj.reactortemp);
+				rcsColdLegTemp.innerHTML = Math.round(obj.rcscoldlegtemp);
+				rcsHotLegTemp.innerHTML = Math.round(obj.rcshotlegtemp);
+				simtime.innerHTML = Math.round(obj.simtime);
+				generalPowerOutput.innerHTML = Math.round(obj.genmw);
+				circulatingSystemColdLegTemp.innerHTML = Math.round(obj.cscoldlegtemp);
+				circulatingSystemHotLegTemp.innerHTML = Math.round(obj.cshotlegtemp);
+				feedwaterColdLegTemp.innerHTML = Math.round(obj.afscoldlegtemp);
+				feedwaterHotLegTemp.innerHTML = Math.round(obj.afshotlegtemp);
+				this.rcspressure.innerHTML=Math.round(obj.rcspressure);
+				this.boilingtemp.innerHTML=Math.round(obj.boilingtemp);
+				this.workers.innerHTML=Math.round(obj.workers);
+				this.risk.innerHTML=Math.round(obj.risk);
+			} catch(err) {
+				console.log("Error in socket:", err);
+			}
 		}
 	});
 });
