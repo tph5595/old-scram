0fc21d86cc22b82f85c683ccc95e0d28906673a2
j105rob <j105.rob@gmail.com>
2013-07-01 12:34:24 -0400

    Updated console display for data

--- http/js/dojo/scram/pump.js
+++ http/js/dojo/scram/pump.js
@@ -1,27 +1,34 @@
-define(["dojo/_base/lang", "dojo/on", "dojo/_base/declare", "dijit/_WidgetBase", "dijit/_Container","dijit/_Contained", "dijit/_TemplatedMixin",
-"dojo/dom-construct", "dojo/dom-class","dojo/text!scram/templates/pump.html"], 
-function(lang, on, Declare, _WidgetBase,_Container, _Contained, _TemplatedMixin, domConstruct, domClass,template) {
+define(["dojo/_base/lang", "dojo/on", "dojo/_base/declare", "dijit/_WidgetBase", "dijit/_Container", "dijit/_Contained", "dijit/_TemplatedMixin", "dojo/dom-construct", "dojo/dom-class", "dojo/text!scram/templates/pump.html"], function(lang, on, Declare, _WidgetBase, _Container, _Contained, _TemplatedMixin, domConstruct, domClass, template) {
 	return Declare("scram.pump", [_WidgetBase, _TemplatedMixin, _Contained, _Container], {
 		///
 		/// This is the class for the pump
 		///
-		templateString:template,
+		templateString : template,
 		socket : null,
-		poll:null,
-		//pump : null,
-		//pumpUp : null,
-		//pumpDown : null,
+		poll : null,
 		pumpLevel : null,
 		pumpLevelMax : null,
 		pumpId : null,
 		pumpClass : null,
-		_setPumpClassAttr: {node:"pump",type:"class"},
+		_setPumpClassAttr : {
+			node : "pump",
+			type : "class"
+		},
 		pumpUpClass : null,
-		_setPumpUpClassAttr: {node:"pumpUp",type:"class"},
+		_setPumpUpClassAttr : {
+			node : "pumpUp",
+			type : "class"
+		},
 		pumpDownClass : null,
-		_setPumpDownClassAttr: {node:"pumpDown",type:"class"},
-		tip:null,
-		_setPumpTipAttr: {node:"pump",type:"title"},
+		_setPumpDownClassAttr : {
+			node : "pumpDown",
+			type : "class"
+		},
+		tip : null,
+		_setPumpTipAttr : {
+			node : "pump",
+			type : "title"
+		},
 
 		constructor : function(args) {
 			this.socket = args.socket;
@@ -32,49 +39,29 @@ function(lang, on, Declare, _WidgetBase,_Container, _Contained, _TemplatedMixin,
 			this.pumpLevel = 0;
 			this.pumpLevelMax = args.pumpLevelMax;
 			this.tip = args.tip;
-			this.socket.on("message", lang.hitch(this, this.pumpMsg));
-			this.poll.on("message",lang.hitch(this,this.pollMsg));
+			this.poll.on("message", lang.hitch(this, this.pollMsg));
 		},
 		postCreate : function() {
-			/*
-			this.pumpDown = new domConstruct.create("div", {
-				'class' : this.pumpDownClass
-			}, this.parent);
-
-			//create the gauge bottom left Down button div
-			this.pumpUp = new domConstruct.create("div", {
-				'class' : this.pumpUpClass
-			}, this.parent);
-
-			//create the gauge bottom left up button div
-			this.pump = new domConstruct.create("div", {
-				'class' : this.pumpClass,
-				'title':this.tip
-			}, this.parent);
-			*/
-			//this.handlePumpDown = on(this.pumpDown, "click", lang.hitch(this, this.pumpUpdate, -1));
-			//this.handlePumpUp = on(this.pumpUp, "click", lang.hitch(this, this.pumpUpdate, 1));
 			this.inherited(arguments);
 		},
-		increment:function(){
+		increment : function() {
 			this.pumpUpdate(1);
 		},
-				decrement:function(){
+		decrement : function() {
 			this.pumpUpdate(-1);
 		},
 		pumpMove : function() {
-			
+			domClass.remove(this.pump);
+			domClass.add(this.pump, 'pump' + this.pumpLevel + ' pumpsize ' + this.pumpClass);
+		},
+		pumpUpdate : function(x) {
+			this.pumpLevel = this.pumpLevel + x
 			if (this.pumpLevel < 0) {
 				this.pumpLevel = 0
 			}
 			if (this.pumpLevel > this.pumpLevelMax) {
 				this.pumpLevel = this.pumpLevelMax;
 			}
-			domClass.remove(this.pump);
-			domClass.add(this.pump, 'pump' + this.pumpLevel + ' pumpsize ' + this.pumpClass);
-		},
-		pumpUpdate : function(x) {
-			this.pumpLevel = this.pumpLevel + x
 			j = {
 				"level" : this.pumpLevel,
 				"pumpid" : this.pumpId
@@ -83,12 +70,7 @@ function(lang, on, Declare, _WidgetBase,_Container, _Contained, _TemplatedMixin,
 			this.socket.send(JSON.stringify(j));
 			this.pumpMove();
 		},
-		pumpMsg : function(event) {
-			var obj = JSON.parse(event.data);
-			this.pumpLevel = obj[this.pumpId];
-			this.pumpMove();
-		},
-		pollMsg:function(event){
+		pollMsg : function(event) {
 			var obj = JSON.parse(event.data);
 			this.pumpLevel = obj[this.pumpId];
 			this.pumpMove();
