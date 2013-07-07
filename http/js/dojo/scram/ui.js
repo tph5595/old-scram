5ecfd6a15fec1335d185673e1a301573792db037
jsr5194 <jsr5194@gmail.com>
2013-07-07 11:31:12 -0400

    moved earthquake setup to ui class

--- http/js/dojo/scram/ui.js
+++ http/js/dojo/scram/ui.js
@@ -1,7 +1,5 @@
-define(["dojo/_base/lang", "dojo/_base/declare", "dijit/_WidgetBase", "dijit/_Container", "dijit/_Contained", 
-"dijit/_TemplatedMixin", "scram/sockets", "scram/plant", "scram/status", "scram/splash", "scram/user", "dojo/text!scram/templates/ui.html"], 
-function(lang, Declare, _WidgetBase, _Container, _Contained, _TemplatedMixin, Sockets, Plant, Status, Splash, User, template) {
-	return Declare("scram.ui", [_WidgetBase, _TemplatedMixin, _Contained, _Container], {
+define(["dojo/on", "dojo/_base/lang", "dojo/_base/declare", "dijit/_WidgetBase", "dijit/_Container", "dijit/_Contained", "dojo/fx", "dijit/_TemplatedMixin", "dojo/Evented", "scram/sockets", "scram/plant", "scram/status", "scram/splash", "scram/user", "scram/earthquake", "dojo/text!scram/templates/ui.html"], function(on, lang, Declare, _WidgetBase, _Container, _Contained, fx, _TemplatedMixin, Evented, Sockets, Plant, Status, Splash, User, Earthquake, template) {
+	return Declare("scram.ui", [_WidgetBase, _TemplatedMixin, Evented, _Contained, _Container], {
 		///
 		/// This is the class for the main UI
 		///
@@ -29,10 +27,35 @@ function(lang, Declare, _WidgetBase, _Container, _Contained, _TemplatedMixin, So
 					this.plant = new Plant({
 						sockets : this._sockets
 					}, this.plantDAP);
+					this.earthquake = new Earthquake({
+						socket : this._sockets.earthquakeSocket,
+						poll : this._sockets.pollSocket
+					}, this.plantDAP);
+					this.earthquake.on('quake', lang.hitch(this, function() {
+						console.log('Quake passthrough works. Now implement shaking here');
+						this.earthquakeLeft = fx.slideTo({
+							node : this.plantDAP,
+							top : 50,
+							left : 86
+						});
+						this.earthquakeRight = fx.slideTo({
+							node : this.plantDAP,
+							top : 500,
+							left : 860
+						});
+						this.earthquakeCenter = fx.slideTo({
+							node : this.plantDAP,
+							top : 100,
+							left : 360
+						});
+						this.chain = fx.chain([this.earthquakeLeft, this.earthquakeRight, this.earthquakeCenter]);
+						this.chain.play();
+					}));
 				}));
 			}));
 			this.addChild(this.splash);
 			this.addChild(this.user);
+			this.addChild(this.earthquake);
 
 			dojo.style(this.userDAP, "opacity", "0");
 			dojo.style(this.statusDAP, "opacity", "0");
@@ -42,7 +65,6 @@ function(lang, Declare, _WidgetBase, _Container, _Contained, _TemplatedMixin, So
 		startup : function() {
 			this.inherited(arguments);
 		}
-		
 	});
 
 });
