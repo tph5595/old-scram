6dc1591a84d1c67b2bde6ab381999dc5dc72da14
jsr5194 <jsr5194@gmail.com>
2013-07-16 11:48:17 -0400

    critical temperatures changing color

--- http/js/dojo/scram/status.js
+++ http/js/dojo/scram/status.js
@@ -1,20 +1,39 @@
-define(["dojo/_base/lang", "dojo/_base/declare", "dijit/_WidgetBase", "dijit/_Container", "dijit/_Contained", 
-"dijit/_TemplatedMixin", "dojo/text!scram/templates/status.html"], function(lang, Declare, _WidgetBase, _Container, _Contained, _TemplatedMixin, template) {
+define(["dojo/_base/lang", "dojo/_base/declare", "dojo/dom-class", "dijit/_WidgetBase", "dijit/_Container", "dijit/_Contained", "dijit/_TemplatedMixin", "dojo/text!scram/templates/status.html"], function(lang, Declare, domClass, _WidgetBase, _Container, _Contained, _TemplatedMixin, template) {
 	return Declare("scram.status", [_WidgetBase, _Container, _Contained, _TemplatedMixin], {
 
 		_socket : null,
 		templateString : template,
-		'class':"statsbarbackground z1",
+		'class' : "statsbarbackground z1",
+		critflag : null,
+		rcsFlag : null,
+		boilingFlag : null,
 		constructor : function(args) {
 			this._socket = args.socket;
 			this._socket.on("message", lang.hitch(this, this.pollMsg));
 		},
 		postCreate : function() {
+			this.rcsFlag = 2;
+			this.boilingFlag = 4;
 			this.inherited(arguments);
 		},
 
 		pollMsg : function(event) {
-
+			var obj = JSON.parse(event.data);
+			this.critflag = obj['critflag'];
+			if ((this.critflag & this.rcsFlag) == this.rcsFlag) {
+				domClass.remove(this.rcsPressure);
+				domClass.add(this.rcsPressure, 'rcspressure criticalstatsbarelements');
+			} else {
+				domClass.remove(this.rcsPressure);
+				domClass.add(this.rcsPressure, 'rcspressure statsbarelements');
+			}
+			if ((this.critflag & this.boilingFlag) == this.boilingFlag) {
+				domClass.remove(this.boilingTemp);
+				domClass.add(this.boilingTemp, 'boilingtemp criticalstatsbarelements');
+			} else {
+				domClass.remove(this.boilingTemp);
+				domClass.add(this.boilingTemp, 'boilingtemp statsbarelements');
+			}
 			try {
 				var obj = JSON.parse(event.data);
 				this.mwh.innerHTML = Math.round(obj.mwh);
@@ -22,10 +41,10 @@ define(["dojo/_base/lang", "dojo/_base/declare", "dijit/_WidgetBase", "dijit/_Co
 				this.boilingTemp.innerHTML = Math.round(obj.boilingtemp);
 				this.workers.innerHTML = Math.round(obj.workers);
 				this.risk.innerHTML = Math.round(obj.risk);
-				
-				var mins = Math.floor(Math.round(obj.simtime)/60%60);
-				var hours = Math.floor(Math.round(obj.simtime)/3660);				
-				var seconds = Math.floor(Math.round(obj.simtime)%60);
+
+				var mins = Math.floor(Math.round(obj.simtime) / 60 % 60);
+				var hours = Math.floor(Math.round(obj.simtime) / 3660);
+				var seconds = Math.floor(Math.round(obj.simtime) % 60);
 				var d = new Date();
 				d.setHours(hours);
 				d.setMinutes(mins);
--- http/js/dojo/scram/temp.js
+++ http/js/dojo/scram/temp.js
@@ -1,22 +1,35 @@
-define(["dojo/_base/lang","dojo/_base/declare", "dijit/_WidgetBase","dijit/_Container", 
+define(["dojo/_base/lang","dojo/_base/declare", "dojo/dom-class", "dijit/_WidgetBase","dijit/_Container", 
 "dijit/_Contained","dijit/_TemplatedMixin","dojo/text!scram/templates/temp.html"], 
-function(lang,Declare, _WidgetBase, _Container,_Contained,_TemplatedMixin, template) {
+function(lang,Declare, domClass, _WidgetBase, _Container,_Contained,_TemplatedMixin, template) {
 	return Declare("scram.temp", [_WidgetBase, _Container,_Contained,_TemplatedMixin], {
 		///
 		/// This is the class for the poll data
 		///
 		_socket : null,
 		templateString:template,
+		critflag : null,
+		reactorFlag : null,
 		constructor : function(args) {
 			this._socket = args.socket;
 			this._socket.on("message", lang.hitch(this, this.pollMsg));
 		},
 		postCreate : function() {
+			this.reactorFlag = 1;
 			this.inherited(arguments);
+			
 		},
 		
 		pollMsg : function(event) {
-
+			var obj = JSON.parse(event.data);
+			this.critflag = obj['critflag'];
+			if ((this.critflag & this.reactorFlag) == this.reactorFlag){
+				domClass.remove(this.reactorTemp);
+				domClass.add(this.reactorTemp, 'reactortemp criticaltemperatureelements');
+			}
+			else {
+				domClass.remove(this.reactorTemp);
+				domClass.add(this.reactorTemp, 'reactortemp temperatureelements');
+			}
 			try {
 				var obj = JSON.parse(event.data);
 				this.reactorTemp.innerHTML = Math.round(obj.reactortemp);
