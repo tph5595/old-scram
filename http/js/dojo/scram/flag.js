1067ad76b8af24ee11f6cd0aaa3d478b027b175c
j105rob <j105.rob@gmail.com>
2013-07-23 10:01:05 -0400

    Updated flag for socket err handling

--- http/js/dojo/scram/flag.js
+++ http/js/dojo/scram/flag.js
@@ -1,19 +1,24 @@
-define(["dojo/_base/lang", "dijit/focus", "dojo/on", "dojo/_base/declare", "dijit/_WidgetBase", "dijit/_Container", "dijit/_Contained", "dijit/_TemplatedMixin", "dojo/Evented", "dojo/dom-construct", "dojo/dom-class", "dijit/form/TextBox", "dojox/socket","dojo/text!scram/templates/flag.html"], function(lang, focusUtil, on, Declare, _WidgetBase, _Container, _Contained, _TemplatedMixin, Evented, domConstruct, domClass, TextBox, Socket, template) {
+define(["dojo/_base/lang", "dijit/focus", "dojo/on", "dojo/_base/declare", "dijit/_WidgetBase", "dijit/_Container", "dijit/_Contained", "dijit/_TemplatedMixin", "dojo/Evented", "dojo/dom-construct", "dojo/dom-class", "dijit/form/TextBox", "dojox/socket", "dojo/text!scram/templates/flag.html"], function(lang, focusUtil, on, Declare, _WidgetBase, _Container, _Contained, _TemplatedMixin, Evented, domConstruct, domClass, TextBox, Socket, template) {
 	return Declare("scram.flag", [_WidgetBase, _TemplatedMixin, _Contained, _Container, Evented], {
 		///
 		/// This is the class for the flag
 		///
 		templateString : template,
-		socket:null,
+		socket : null,
 		constructor : function(args) {
-			this.socket = new Socket("ws://127.0.0.1:50506");
+			this.socket = new Socket("ws://192.168.15.5:50506");
+			this.socket.on("error", lang.hitch(this, function(e) {
+				console.log("Flag Socket Error", e);
+			}));
 		},
 		postCreate : function() {
 			this.inherited(arguments);
 		},
 		onFlagSubmission : function() {
-			var submitFlag = {'flag':this.flagDAP.value};
-			console.log("Flag Post Data",submitFlag);
+			var submitFlag = {
+				'flag' : this.flagDAP.value
+			};
+			console.log("Flag Post Data", submitFlag);
 			this.socket.send(JSON.stringify(submitFlag));
 		}
 	});
