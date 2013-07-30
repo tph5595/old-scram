14509659c8a5baafcfa81a2e71059aca6ca19cf5
Joe Pecsi <jpecsi@me.com>
2013-07-30 13:16:05 -0400

    Changed socket stuff for testing

--- http/js/dojo/scram/sockets.js
+++ http/js/dojo/scram/sockets.js
@@ -12,37 +12,37 @@ define(["dojo/_base/lang", "dojo/_base/declare", "dojox/socket","dojo/Evented"],
 		earthquakeSocket : null,
 		constructor : function(args) {
 			//TODO: add emits for events
-			this.pollSocket = new Socket("ws://127.0.0.1:8081");
+			this.pollSocket = new Socket("ws://192.168.6.200:8081");
 			this.pollSocket.on("message", lang.hitch(this, this.onPollMsg));
 			this.pollSocket.on("open", lang.hitch(this, this.onPollOpen));
 			this.pollSocket.on("close", lang.hitch(this, this.onPollClose));
 			this.pollSocket.on("error", lang.hitch(this, this.onPollErr));
 
-			this.valveSocket = new Socket("ws://127.0.0.1:8082");
+			this.valveSocket = new Socket("ws://192.168.6.200:8082");
 			this.valveSocket.on("message", lang.hitch(this, this.onValveMsg));
 			this.valveSocket.on("open", lang.hitch(this, this.onValveOpen));
 			this.valveSocket.on("close", lang.hitch(this, this.onValveClose));
 			this.valveSocket.on("error", lang.hitch(this, this.onValveErr));
 
-			this.pumpSocket = new Socket("ws://127.0.0.1:8083");
+			this.pumpSocket = new Socket("ws://192.168.6.200:8083");
 			this.pumpSocket.on("message", lang.hitch(this, this.onPumpMsg));
 			this.pumpSocket.on("open", lang.hitch(this, this.onPumpOpen));
 			this.pumpSocket.on("close", lang.hitch(this, this.onPumpClose));
 			this.pumpSocket.on("error", lang.hitch(this, this.onPumpErr));
 
-			this.rodSocket = new Socket("ws://127.0.0.1:8084");
+			this.rodSocket = new Socket("ws://192.168.6.200:8084");
 			this.rodSocket.on("message", lang.hitch(this, this.onRodMsg));
 			this.rodSocket.on("open", lang.hitch(this, this.onRodOpen));
 			this.rodSocket.on("close", lang.hitch(this, this.onRodClose));
 			this.rodSocket.on("error", lang.hitch(this, this.onRodErr));
 			
-			this.userSocket = new Socket("ws://127.0.0.1:8085");
+			this.userSocket = new Socket("ws://192.168.6.200:8085");
 			this.userSocket.on("message", lang.hitch(this, this.onUserMsg));
 			this.userSocket.on("open", lang.hitch(this, this.onUserOpen));
 			this.userSocket.on("close", lang.hitch(this, this.onUserClose));
 			this.userSocket.on("error", lang.hitch(this, this.onUserErr));
 			
-			this.earthquakeSocket = new Socket("ws://127.0.0.1:8086");
+			this.earthquakeSocket = new Socket("ws://192.168.6.200:8086");
 			this.earthquakeSocket.on("message", lang.hitch(this, this.onEarthquakeMsg));
 			this.earthquakeSocket.on("open", lang.hitch(this, this.onEarthquakeOpen));
 			this.earthquakeSocket.on("close", lang.hitch(this, this.onEarthquakeClose));
